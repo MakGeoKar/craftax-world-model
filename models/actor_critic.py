c50e7ed70b0b3bd3164fcd10ae7abef920d1aa8a
MakGeoKar <makoyan2008@gmail.com>
2026-05-26 14:07:06 +0300

    Add actor critic world model architecture

--- models/actor_critic.py
+++ models/actor_critic.py
@@ -1,3 +1,4 @@
+import jax
 import jax.numpy as jnp
 import flax.linen as nn
 import numpy as np
@@ -124,6 +125,169 @@ class ActorCriticConv(nn.Module):
         )
 
         return pi, jnp.squeeze(critic, axis=-1)
+
+
+class ActorCriticConvWorldModel(nn.Module):
+    action_dim: Sequence[int]
+    layer_width: int
+    activation: str = "tanh"
+
+    def setup(self):
+        self.enc_conv1 = nn.Conv(
+            features=32, kernel_size=(5, 5), name="enc_conv1"
+        )
+        self.enc_conv2 = nn.Conv(
+            features=32, kernel_size=(5, 5), name="enc_conv2"
+        )
+        self.enc_conv3 = nn.Conv(
+            features=32, kernel_size=(5, 5), name="enc_conv3"
+        )
+        self.enc_latent = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="enc_latent",
+        )
+
+        self.actor_fc1 = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="actor_fc1",
+        )
+        self.actor_fc2 = nn.Dense(
+            self.action_dim,
+            kernel_init=orthogonal(0.01),
+            bias_init=constant(0.0),
+            name="actor_fc2",
+        )
+        self.actor_out = nn.Dense(
+            self.action_dim,
+            kernel_init=orthogonal(0.01),
+            bias_init=constant(0.0),
+            name="actor_out",
+        )
+
+        self.critic_fc1 = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="critic_fc1",
+        )
+        self.critic_out = nn.Dense(
+            1,
+            kernel_init=orthogonal(1.0),
+            bias_init=constant(0.0),
+            name="critic_out",
+        )
+
+        self.wm_fc1 = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="wm_fc1",
+        )
+        self.wm_fc2 = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="wm_fc2",
+        )
+        self.wm_next_latent = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(1.0),
+            bias_init=constant(0.0),
+            name="wm_next_latent",
+        )
+        self.wm_reward = nn.Dense(
+            1,
+            kernel_init=orthogonal(1.0),
+            bias_init=constant(0.0),
+            name="wm_reward",
+        )
+        self.wm_done = nn.Dense(
+            1,
+            kernel_init=orthogonal(1.0),
+            bias_init=constant(0.0),
+            name="wm_done",
+        )
+
+        self.inv_fc1 = nn.Dense(
+            self.layer_width,
+            kernel_init=orthogonal(2),
+            bias_init=constant(0.0),
+            name="inv_fc1",
+        )
+        self.inv_action = nn.Dense(
+            self.action_dim,
+            kernel_init=orthogonal(0.01),
+            bias_init=constant(0.0),
+            name="inv_action",
+        )
+
+    def encode(self, obs):
+        x = self.enc_conv1(obs)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+        x = self.enc_conv2(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+        x = self.enc_conv3(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+
+        x = x.reshape(x.shape[0], -1)
+        latent = self.enc_latent(x)
+        latent = nn.relu(latent)
+        return latent
+
+    def __call__(self, obs):
+        latent = self.encode(obs)
+
+        actor_x = self.actor_fc1(latent)
+        actor_x = nn.relu(actor_x)
+        actor_x = self.actor_fc2(actor_x)
+        actor_x = nn.relu(actor_x)
+        actor_logits = self.actor_out(actor_x)
+        pi = distrax.Categorical(logits=actor_logits)
+
+        critic_x = self.critic_fc1(latent)
+        critic_x = nn.relu(critic_x)
+        value = self.critic_out(critic_x)
+
+        return pi, jnp.squeeze(value, axis=-1), latent
+
+    def world_model(self, latent, action):
+        action_onehot = jax.nn.one_hot(action, self.action_dim)
+        x = jnp.concatenate([latent, action_onehot], axis=-1)
+        x = self.wm_fc1(x)
+        x = nn.relu(x)
+        x = self.wm_fc2(x)
+        x = nn.relu(x)
+
+        pred_next_latent = self.wm_next_latent(x)
+        pred_reward = self.wm_reward(x)
+        pred_done_logit = self.wm_done(x)
+
+        return (
+            pred_next_latent,
+            jnp.squeeze(pred_reward, axis=-1),
+            jnp.squeeze(pred_done_logit, axis=-1),
+        )
+
+    def inverse_model(self, latent, next_latent):
+        x = jnp.concatenate([latent, next_latent], axis=-1)
+        x = self.inv_fc1(x)
+        x = nn.relu(x)
+        pred_action_logits = self.inv_action(x)
+        return pred_action_logits
+
+    def init_all(self, obs, action, next_obs):
+        pi, value, latent = self(obs)
+        _, _, next_latent = self(next_obs)
+        self.world_model(latent, action)
+        self.inverse_model(latent, next_latent)
+        return pi, value, latent
 
 
 class ActorCritic(nn.Module):
